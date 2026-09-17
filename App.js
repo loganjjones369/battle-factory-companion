c6d54da3773a0d90092991a69fc8479585623741
loganjjones369 <loganjacobjones@gmail.com>
2026-09-17 11:32:36 -0400

    Restore main app flow after matchup lab integration work

--- App.js
+++ App.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import { ScrollView } from 'react-native';
-import RunFlowV3 from './RunFlowV3';
-import DraftMatchupLab from './components/DraftMatchupLab';
-
-export default function App() {
-  return <ScrollView keyboardShouldPersistTaps="handled">
-    <DraftMatchupLab level="Level 50" battle={1} />
-    <RunFlowV3 />
-  </ScrollView>;
-}
+export { default } from './RunFlowV3';
