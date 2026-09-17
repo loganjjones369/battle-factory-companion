980ac87d281dcb7af2156726c7b783f9cb557a56
loganjjones369 <loganjacobjones@gmail.com>
2026-09-17 12:06:23 -0400

    Add live exact-set draft survival panel

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, View } from 'react-native';
 import RunFlowV3 from './RunFlowV3';
 import ScientistCluePanel from './components/ScientistCluePanel';
 import ThreatDashboard from './components/ThreatDashboard';
+import DraftSurvivalDock from './components/DraftSurvivalDock';
 import { getCurrentScientist, setCurrentScientist } from './data/scientistSession';
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
     <View style={styles.scientistDock}>
       <ScientistCluePanel scientist={scientist} onChange={updateScientist} />
       <ThreatDashboard />
+      <DraftSurvivalDock />
     </View>
     <View style={styles.flow}>
       <RunFlowV3 />
