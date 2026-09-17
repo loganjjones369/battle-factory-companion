020f310735be30d728538ef755ab111c0eca6f70
loganjjones369 <loganjacobjones@gmail.com>
2026-09-17 11:32:49 -0400

    Keep any-Pokemon matchup lab accessible above the main Battle Factory flow

--- App.js
+++ App.js
@@ -1,1 +1,11 @@
-export { default } from './RunFlowV3';
+import React from 'react';
+import { View } from 'react-native';
+import RunFlowV3 from './RunFlowV3';
+import DraftMatchupLab from './components/DraftMatchupLab';
+
+export default function App() {
+  return <View style={{ flex: 1 }}>
+    <DraftMatchupLab level="Level 50" battle={1} />
+    <RunFlowV3 />
+  </View>;
+}
